3d8a7c2a6831beba9b0ce89444cc848b25e84730
troniixx <meroert55@gmail.com>
2021-09-29 16:02:36 +0200

    task 2.4 done, rest on access

---
+++ taskTwoFour.py
@@ -0,0 +1,14 @@
+s = "aB:cD"
+
+print("Start string = " + s)
+
+a, b = s.split(":", 1)
+
+print("String split at : and assigned to new variables;")
+print(a)
+print(b)
+
+z = a.lower()
+y = b.upper()
+
+print(z + ":" + y)
