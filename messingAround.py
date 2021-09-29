3102b7fa1b15097069113b458a019296b53ffa2d
troniixx <meroert55@gmail.com>
2021-09-29 17:13:10 +0200

    dumb shit

---
+++ messingAround.py
@@ -0,0 +1,17 @@
+print("playing with tables")
+
+""""
+x = [[0/0, 0/1, 0/2],
+     [1/0, 1/1, 1/2],
+     [2/0, 2/1, 2/2],
+     [3/0, 3/1, 3/2 ]]
+
+"""""
+
+x = [["Peter", "Mueller", "Max"],
+     ["Mert", "Mike", "Zuerich"],
+     ["Marko", "Dominik", "Davor"],
+     ["Ardian", "Karoline", "Damian"]]
+
+print(x[2][1])
+print(x[3][2])
