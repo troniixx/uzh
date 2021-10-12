4e8d9510c63a344c6ef36dbf9394b71d74c876c0
troniixx <meroert55@gmail.com>
2021-10-12 19:27:32 +0200

    created a gcd finder using euclid and recursion and a program that tells you if a # is prime or not

---
+++ Informatik1/gcd_recursion.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+
+# This signature is required for the automated grading to work.
+# Do not rename the function or change its list of parameters!
+def absolute_value(a):
+
+    # implement this function
+    return abs(a)
+
+# This signature is required for the automated grading to work.
+# Do not rename the function or change its list of parameters!
+def gcd(a, b):
+
+    # handle negative numbers
+    if a < 0:
+        a = absolute_value(a)
+
+    if b < 0:
+        b = absolute_value(b)
+
+    # constraints
+    if a == 0:
+        if (a == 0) and (b == 0):
+            return None
+        return absolute_value(b)
+    elif b == 0:
+        if (a == 0) and (b == 0):
+            return None
+        return absolute_value(a)
+
+    # euclid
+    if b > a:
+        return gcd(b, a)
+    if a % b == 0:
+        return b
+    return gcd(b, a % b)
+
+# The following line calls the function and prints the return
+# value to the Console. This way you can check what it does.
+# However, we encourage you to write tests, because then you
+# can easily test many different values on every "Test & Run"!
+a = 0
+b = -12348
+print(f"greatest common divisor of {a} and {b} is = {gcd(a, b)}")
