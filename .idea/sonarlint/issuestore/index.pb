3d9add1076655c487236248225b542df7fe7fd22
troniixx <meroert55@gmail.com>
2021-10-14 18:36:37 +0200

    done

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,16 @@
 
 ^
 ..idea/inspectionProfiles/profiles_settings.xml,1/e/1e9075f5bf079c01ef2c910709e91a497d262080
+L
+Informatik1/passwordvalid.py,e/9/e9831268ce383d3e665d51de714befbc605a364a
+J
+Infk&Wirt/messingAround.py,a/2/a23dd2fffb5e5a704db875a64fb42a5cf769192f
+G
+Informatik1/taskOne4.py,4/9/497c5e97d4a1233fd4d47a728d63354677f57eb5
+J
+Informatik1/taskTwoFour.py,f/1/f14bb005352bde3c02cd24d380fbbfcddd23ec0e
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- Informatik1/pyramidScheme.py
+++ Informatik1/pyramidScheme.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # You can freely adopt this number to print pyramids of different sizes
-h = 4
+h = 0
 
 # build a string
 def build_string_pyramid():
@@ -10,21 +10,21 @@ def build_string_pyramid():
     # at the end of the function.
     s = ""
 
-    if h == 0:
-        s = ""
+    for i in range(h):
+        for j in range(i + 1):
+            if j == max(range(i + 1)):
+                s += str(j + 1)
+            else:
+                s += str(j + 1) + "*"
+        s += "\n"
 
-    #obere haelfte
-
-
-        # untere hälfte
-    for i in range(0, h):
-        c = 1
-        print(c, end=" ")
-        for j in range(h - i - 1, 0, -1):
-            print('*', end=" ")
-            c = c + 1
-            print(c, end=" ")
-        print("\n")
+    for i in range(h - 1, 0, -1):
+        for j in range(1, i + 1):
+            if j == max(range(1, i + 1)):
+                s += str(j)
+            else:
+                s += str(j) + "*"
+        s += "\n"
 
     return s
 
@@ -32,30 +32,6 @@ def build_string_pyramid():
 
     # Enter your code here
     # use nested loops and the range() function
-    """
-    for i in range(1, h + 1):
-        for j in range(1, i + 1):
-            print(str(j)+"*", end = " ")
-        print(" ")
-
-    for i in range(1, h + 1):
-        for j in range(h - i, 0, -1):
-            print(str(j)+"*", end=" ")
-        print(" ")
-"""
-
-"""
-#obere hälfte
-    for i in range(0, h):
-        e = 1
-        print(e, end = " ")
-        for j in range(1, h + i + 1):
-            print("*", end = " ")
-            e = e + 1
-            print(e, end = " ")
-        print("\n")
-"""
-
 
     # You don't need to change the following line.
     # It simply returns the string created above
