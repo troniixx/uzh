c5e4231506b8aed370ca4576e4282b29b9ffd9e3
merterol <meroert55@gmail.com>
2021-10-23 18:25:42 +0200

    got to learn how dicts work

--- Informatik1/Ex 5/reverse_indexing.py
+++ Informatik1/Ex 5/reverse_indexing.py
@@ -4,14 +4,34 @@ from collections import defaultdict
 dataset = [
     "Hello world",
     "This is the WORLD",
-    "hello again"
- ] 
+    "hello again"] 
 
 def reverse_index(dataset):
     
     index_dictionary = {}
 
+    #lower casing the whole set
+    lower_case_dataset = []
+    for element in dataset:
+        lower_case_dataset.append(element.lower())
+    print(lower_case_dataset)
+
+    #new list with each word seperated
+    seperated = []
+    for elements in lower_case_dataset:
+        seperated.append(elements.split(' '))
+    print(seperated)
+
+    #add to dictionary
+    for sublist in seperated:
+        pass
+            
+        
+
+
+
     # don't forget to return your resulting dictionary
+    return index_dictionary
 
 # You can see the output of your function here
 print(reverse_index(dataset))
