82dc0be994b62f53d5fbc2abebe8a6e82bd5cda2
merterol <meroert55@gmail.com>
2021-10-24 15:05:15 +0200

    bugfixing

--- Informatik1/Ex 5/hashtag_analysis.py
+++ Informatik1/Ex 5/hashtag_analysis.py
@@ -3,9 +3,18 @@
 # This signature is required for the automated grading to work. 
 # Do not rename the function or change its list of parameters.
 def analyze(posts):
-    pass
 
+    result = {}
 
+    for item in posts:
+        for word in item.split(): #split element into singular words
+            if word.startswith("#") and len(word) > 1: #if word starts with # and isnt len <= 0
+                if word.replace("#", "") not in result: #check if word isnt in dict
+                    result[word.replace("#", "")] = 1   #if not in dict add id with value 1
+                else:
+                    result[word.replace("#", "")] += 1 #if it is in list, add 1 to the value
+
+    return result
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
 # However, we encourage you to write tests, because then you
@@ -14,5 +23,7 @@ posts = [
     "hi #weekend",
     "good morning #zurich #limmat",
     "spend my #weekend in #zurich",
-    "#zurich <3"]
+    "#zurich <3 #",
+    ".#c.", "#ZURICH"]
+
 print(analyze(posts))
