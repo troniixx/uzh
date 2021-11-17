8fd8da5e7b8a4cbe613c8eb3d4783add9e6cef63
merterol <meroert55@gmail.com>
2021-11-17 12:03:43 +0100

    things

---
+++ Informatik1/Ex 9/profanity.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# The signatures of this class and its public methods are required for the automated grading to work. 
+# You must not change the names or the list of parameters. 
+# You may introduce private/protected utility methods though.
+class ProfanityFilter:
+
+    def __init__(self, keywords, template):
+        self.keywords = keywords
+        self.template = template
+
+    def filter(self, msg):
+        pass
+
+    def __clean(self, word):
+        pass
+
+
+# You can play around with your implementation in the body of the following 'if'.
+# The contained statements will be ignored while evaluating your solution.
+if __name__ == '__main__':
+    f = ProfanityFilter(["duck", "shot", "batch", "mastard"], "?#$")
+    offensive_msg = "abc defghi mastard jklmno"
+    clean_msg = f.filter(offensive_msg)
+    print(clean_msg)  # abc defghi ?#$?#$? jklmno
