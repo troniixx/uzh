afe7feb09a2d075f85eabdd0c5e7c4936a85f277
merterol <meroert55@gmail.com>
2021-11-17 12:57:38 +0100

    new exset prep

--- Informatik1/Ex 9/fridge.py
+++ Informatik1/Ex 9/fridge.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python3
 
 class Fridge:
+
+    def __init__(self, best_before, name):
+        self.name = name
+        self.best_before = best_before
+    
+    def __iter__(self): # iterate over the contents ordered by eat-by date
+        pass
+
+    def __next__(self):
+        pass
+
+    def __len__(self): #check how many items are in the fridge
+        pass
+
+    def find(name):
+        pass
+
+    def take_before(date):
+        pass
+
+
+if __name__ == '__main__':
     pass
+
 
