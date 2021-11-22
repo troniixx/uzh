c201b3b4604cdccea93ab93caea76e0541006c1c
merterol <meroert55@gmail.com>
2021-11-22 12:53:38 +0100

    made classic fizzbuzz

--- Java/Ex 2/fizzbuzz.java
+++ Java/Ex 2/fizzbuzz.java
@@ -1,5 +1,18 @@
 public class fizzbuzz {
     public static void main(String[] args) {
-        System.out.println("Test");
+        int n = 50;
+
+        if( n % 3 == 0 && n % 5 != 0){
+            System.out.println("Fizz");
+        }
+        else if(n % 5 == 0 && n % 3 != 0){
+            System.out.println("Buzz"); 
+        }
+        else if(n % 3 == 0 && n % 5 == 0){
+            System.out.println("FizzBuzz");
+        }
+        else {System.out.println(n);
+        }
+
     }
 }
