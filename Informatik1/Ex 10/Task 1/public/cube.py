b58b3809ff01cc31667b9723598a9b39d49b9acf
merterol <meroert55@gmail.com>
2021-11-23 16:59:50 +0100

    started on geometric objects

--- Informatik1/Ex 10/Task 1/public/cube.py
+++ Informatik1/Ex 10/Task 1/public/cube.py
@@ -2,4 +2,17 @@ from public.geometric_object import GeometricObject
 
 
 class Cube(GeometricObject):
-    pass
+    
+    def __init__(self, side_length, color, filled):
+        self.__side_length = side_length
+        self.color = color
+        self.filled = filled
+
+    def get_side_length(self):
+        return self.__side_length
+
+    def get_area(self):
+        return 6* (self.__side_length**2)
+
+    def get_volume(self):
+        return self.__side_length**3
--- Informatik1/Ex 10/Task 1/public/cylinder.py
+++ Informatik1/Ex 10/Task 1/public/cylinder.py
@@ -2,4 +2,22 @@ from public.geometric_object import GeometricObject
 
 
 class Cylinder(GeometricObject):
-    pass
+    
+    def __init__(self, radius, height, color, filled):
+        self.__PI = 3.14
+        self.__radius = radius
+        self.__height = height
+        self.color = color
+        self.filled = filled
+
+    def get_radius(self):
+        return self.__radius
+
+    def get_height(self):
+        return self.__height
+
+    def get_area(self):
+        return (self.__PI * (self.__radius**2)) * (2*self.__PI*self.__radius*self.__height)
+
+    def get_volume(self):
+        return self.__PI*(self.__radius**2)*self.__height
