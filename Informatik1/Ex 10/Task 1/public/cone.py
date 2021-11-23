3caacb2a786f69eeebd2860bae3ad9a125f0557a
merterol <meroert55@gmail.com>
2021-11-23 16:59:39 +0100

    started on geometric objects

--- Informatik1/Ex 10/Task 1/public/cone.py
+++ Informatik1/Ex 10/Task 1/public/cone.py
@@ -2,4 +2,27 @@ from public.geometric_object import GeometricObject
 
 
 class Cone(GeometricObject):
-    pass
+
+    def __init__(self, radius, vertical_height, slant_height, color, filled):
+        self.__PI = 3.14
+        self.__radius = radius
+        self.__vertical_height = vertical_height
+        self.__slant_height = slant_height
+        self.color = color
+        self.filled = filled
+
+    def get_radius(self):
+        return self.__radius
+
+    def get_vertical_height(self):
+        return self.__vertical_height
+
+    def get_slant_height(self):
+        return self.__slant_height
+
+    def get_area(self):
+        return (self.__PI * (self.__radius**2)) * (self.__PI * self.__radius * self.__slant_height)
+
+    def get_volume(self):
+        x = 1/3
+        return x * self.__PI * (self.__radius**2) * self.__vertical_height
--- Informatik1/Ex 10/Task 1/public/geometric_object.py
+++ Informatik1/Ex 10/Task 1/public/geometric_object.py
@@ -2,4 +2,29 @@ from abc import ABC, abstractmethod
 
 
 class GeometricObject:
-    pass
+    
+    #color = str, filled = bool
+
+    def __init__(self, color, filled):
+        self.__color = color
+        self.__filled = filled
+
+    def get_color(self):
+        return self.__color
+
+    def set_color(self, color):
+        pass
+
+    def get_filled(self):
+        return self.__filled
+
+    def set_filled(self, filled):
+        pass
+
+    @abstractmethod
+    def get_area(self): #float
+        pass
+
+    @abstractmethod
+    def get_volume(self): #float
+        pass
