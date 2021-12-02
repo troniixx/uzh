d36bf561360a3513f5f471febbd7f37c4d21706f
merterol <meroert55@gmail.com>
2021-12-02 15:32:42 +0100

    too dumb for this

---
+++ Informatik1/Ex 11/branches_merging.sh
@@ -0,0 +1,24 @@
+# Variables can either be defined and directly used...
+SOME_VAR="Hello World!"
+echo $SOME_VAR
+# or provided during script invocation like in this task. You
+# can use the REPO_URL variable that points to a repository.
+
+# Add your terminal commands here. Make sure to first run them
+# locally on your machine to have more detailed error output.
+git clone $REPO_URL repo
+cd repo
+git branch feature_x
+git checkout feature_x
+printf "print('Hello World!')" > hello.py
+git add hello.py
+git commit -m "Add 'Hello World' example"
+git push origin feature_x
+checkout master
+printf "# goodbye" > bye.py
+git add bye.py
+git commit -m "Add 'Good Bye' comment"
+git merge -m feature_x
+git push
+
+
