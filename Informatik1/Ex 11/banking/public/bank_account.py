9bd75be8c29fdd616b558bf343da034c49e3a8ee
merterol <meroert55@gmail.com>
2021-12-05 17:28:40 +0100

    done

--- Informatik1/Ex 11/banking/public/bank_account.py
+++ Informatik1/Ex 11/banking/public/bank_account.py
@@ -5,16 +5,26 @@ from exchange_rates import EXCHANGE_RATES as er
 class BankAccount:
 
     def __init__(self, currency="CHF"):
-        pass
+        if currency not in er: raise Warning("invalid")
+        self.__currency = currency
+        self.__balance = 0
 
     def get_currency(self):
-        pass
+        return self.__currency
 
     def get_balance(self):
-        pass
+        return self.__balance
         
     def deposit(self, amount, currency="CHF"):
-        pass
+        try: convert(amount, currency, currency)
+        except Warning: raise Warning
+
+        self.__balance += convert(amount, currency, self.__currency)
     
     def withdraw(self, amount, currency="CHF"):
-        pass
+        try: convert(amount, currency, currency)
+        except Warning: raise Warning
+
+        if self.get_balance() < convert(amount, currency, self.__currency):
+            raise Warning("funds not sufficient")
+        self.__balance -= convert(amount, currency, self.__currency)
--- Informatik1/Ex 11/banking/public/currency_converter.py
+++ Informatik1/Ex 11/banking/public/currency_converter.py
@@ -3,4 +3,23 @@
 from exchange_rates import EXCHANGE_RATES as er
 
 def convert(amount, from_currency, to_currency):
-    pass
+    
+    if from_currency not in er or to_currency not in er:
+        raise Warning("invalid currency")
+    if not isinstance(amount, (float, int)):
+        raise Warning("invalid amount")
+    if amount < 0: raise Warning("negative amount")
+
+    if from_currency == to_currency:
+        return amount
+    else:
+        for i in er:
+            if i == from_currency:
+                try: rate = er[from_currency][to_currency]
+                except KeyError: continue
+            
+            elif i == to_currency:
+                try: rate = 1 / er[to_currency][from_currency]
+                except KeyError: continue
+    
+    return amount * rate
