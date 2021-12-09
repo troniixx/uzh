69007675376dea17339f3505c39cd800d70fce94
merterol <meroert55@gmail.com>
2021-12-09 12:33:43 +0100

    dababy

--- Informatik1/Ex 12/word_similarity.py
+++ Informatik1/Ex 12/word_similarity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import random
-
+from difflib import SequenceMatcher
+from math import floor
 class WordLogic(object):
 
     def __init__(self, num_words, len_words):
@@ -13,9 +14,22 @@ class WordLogic(object):
             word_list = f.read().splitlines()
         return [word.upper() for word in word_list if len(word) is self.len_words]
 
+    def is_similar(self, a, b, threshold):
+        if SequenceMatcher(None, a, b).ratio() > threshold:
+            return True
+        else: return False
+
     def word_selection(self):
         words = self.find_words_with_right_size()
         random.shuffle(words)
+        pool = floor(self.num_words/3)
+        selected = random.choice(words[:pool])
+        pool2 = words[pool:]
+        res = [selected]
 
+        while len(res) < self.num_words:
+            t = random.choice(pool2)
+            if t not in res and self.is_similar(selected, t, 0.4):
+                res.append(t)
         # TODO: instead of returning a random sample of words, use the strategy described in task 2
-        return words[0:self.num_words]
+        return res
