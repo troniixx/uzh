0a5f09acdcaff2a272e27d2ebcc2a336722b0431
merterol <meroert55@gmail.com>
2021-12-09 13:04:42 +0100

    hs19

--- Informatik1/Finals Prep/HS19/hailstone.py
+++ Informatik1/Finals Prep/HS19/hailstone.py
@@ -1,15 +1,19 @@
 def hail_me_daddy(num):
     res = []
     res.append(int(num))
-
-    while num != 1:
-        if num%2 == 0:
-            num = num/2
-            res.append(int(num))
-        else:
-            num = 3*num + 1
-            res.append(int(num))
+    
+    if num == 0:
+        return []
+    else:
+        while num != 1:
+            if num%2 == 0:
+                num = num/2
+                res.append(int(num))
+            else:
+                num = 3*num + 1
+                res.append(int(num))
     
     return res
 
+print(hail_me_daddy(0))
 print(hail_me_daddy(12))
--- Informatik1/Finals Prep/HS19/task1.md
+++ Informatik1/Finals Prep/HS19/task1.md
@@ -8,4 +8,22 @@ c)
 Type: str, Value: "1.83"
 
 d)
-Type: 
+Type: list, Value: [1, 4]
+
+e)
+Type: Error, Value: Error
+
+f)
+Type: Error, Value: Method not defined
+
+g)
+Type: int, Value: 1
+
+h)
+Type: float, Value: 6.0
+
+i)
+Type: str, Value: "fin"
+
+j)
+Type: Error, Value: Error
