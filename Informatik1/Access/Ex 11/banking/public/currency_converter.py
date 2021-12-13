40ff20d175577f2e660ad06f6ed70a8e05489587
Mert Erol <71695384+troniixx@users.noreply.github.com>
2021-12-13 18:19:03 +0100

    Update currency_converter.py

--- Informatik1/Access/Ex 11/banking/public/currency_converter.py
+++ Informatik1/Access/Ex 11/banking/public/currency_converter.py
@@ -1,25 +1,20 @@
-#!/usr/bin/env python3
-# add imports, if necessary
-from exchange_rates import EXCHANGE_RATES as er
+from public.exchange_rates import EXCHANGE_RATES as er
 
 def convert(amount, from_currency, to_currency):
-    
-    if from_currency not in er or to_currency not in er:
-        raise Warning("invalid currency")
-    if not isinstance(amount, (float, int)):
-        raise Warning("invalid amount")
-    if amount < 0: raise Warning("negative amount")
 
-    if from_currency == to_currency:
-        return amount
-    else:
-        for i in er:
-            if i == from_currency:
-                try: rate = er[from_currency][to_currency]
-                except KeyError: continue
-            
-            elif i == to_currency:
-                try: rate = 1 / er[to_currency][from_currency]
-                except KeyError: continue
-    
+    if from_currency not in er or to_currency not in er:
+        raise Warning('invalid currency')
+    if not isinstance(amount, (float, int)):
+        raise Warning('invalid amount')
+    if amount < 0: raise Warning('negative amount')
+
+    if from_currency == to_currency: return amount
+    for i in er:
+        if i == from_currency:
+            try: rate = er[from_currency][to_currency]
+            except KeyError: continue
+        elif i == to_currency:
+            try: rate = 1 / er[to_currency][from_currency]
+            except KeyError: continue
+
     return amount * rate
