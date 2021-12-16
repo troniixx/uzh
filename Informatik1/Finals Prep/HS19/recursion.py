68200468f33170d06328912def7fe227b1833e30
merterol <meroert55@gmail.com>
2021-12-16 10:00:45 +0100

    SaveTheTrees

--- Informatik1/Finals Prep/HS19/recursion.py
+++ Informatik1/Finals Prep/HS19/recursion.py
@@ -9,5 +9,16 @@ root = Node(10, \
     Node(15, Node(8)))
 
 def range_sum(node, lower, upper):
-    sum = 0
+    res = 0
+    if lower <= node.v < upper:
+        res += node.v
+    if node.l != None:
+        res += range_sum(node.l, lower, upper)
+    if node.r != None:
+        res += range_sum(node.r, lower, upper)
     
+    return res
+
+assert range_sum(Node(7), 1, 100) == 7
+assert range_sum(Node(2, Node(3, Node(4))), 2, 4) == 5
+assert range_sum(root, 4, 10) == 13 # see example above
