5c6eade93308b3f3017dba558992cd441da8e6c7
merterol <meroert55@gmail.com>
2021-12-21 20:13:19 +0100

    dead inside

---
+++ Informatik1/Finals Prep/messing_around.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+class Bus(Vehicle):
+    
+    def seating_capacity(self, capacity = 50):
+        return super().seating_capacity(capacity = 50)
