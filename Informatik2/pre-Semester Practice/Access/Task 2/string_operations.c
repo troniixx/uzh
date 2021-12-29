947c402555764fb94808b405028df48b0680de5b
merterol <meroert55@gmail.com>
2021-12-29 16:47:46 +0100

    i have a deathwish

---
+++ Informatik2/pre-Semester Practice/Access/Task 2/string_operations.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    char code[] = "aB:cD";
+
+    for(i = 0; i <= sizeof(code); i++){
+
+    }
+    
+}
---
+++ Informatik2/pre-Semester Practice/Hackerrank/sum_diff.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a;
+    int b;
+    float c;
+    float d;
+
+    scanf("%d %d", &a, &b);
+    scanf("%f %f", &c, &d);
+    
+    printf("%d %d", a+b, a-b);
+    printf("\n");
+    printf("%.1f %.1f", c+d, c-d);
+    
+    return 0;
+}
