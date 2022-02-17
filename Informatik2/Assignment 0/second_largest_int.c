c54438650bcaa735d4e13f53c3c119a2bfe05e19
merterol <meroert55@gmail.com>
2022-02-17 13:13:19 +0100

    Assignment 0 done

--- Informatik2/Assignment 0/second_largest_int.c
+++ Informatik2/Assignment 0/second_largest_int.c
@@ -2,8 +2,35 @@
 
 int main() {
 
-    int min;
+    int i, j, temp;
     int Arr[] = {0, 1, 2, 3, 4, 5, 6, 7};
+    int n = 8;
     
+    /* first sort the array from largest to smallest and then output the second number */
+
+    /* sort */
     
+    for(i = 0; i < n; ++i){
+        for(j = i + 1; j < n; ++j) {
+
+            if (Arr[i] < Arr[j]) {
+
+                temp = Arr[i];
+                Arr[i] = Arr[j];
+                Arr[j] = temp;
+
+            }
+        }
+    }
+
+    /* print the array */
+    for(i = 0; i < n; ++i) {
+        printf("%d\n", Arr[i]);
+    }
+
+    /* print the result */
+
+    printf("%d\n", Arr[1]);
+    return 0;
+
 }
