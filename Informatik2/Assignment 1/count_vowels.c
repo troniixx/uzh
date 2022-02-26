35cccaf910b95d15db21be80c8567eb427202591
merterol <meroert55@gmail.com>
2022-02-26 11:10:08 +0100

    on the verge of dying

--- Informatik2/Assignment 1/count_vowels.c
+++ Informatik2/Assignment 1/count_vowels.c
@@ -18,8 +18,20 @@ int count_vowels(char A[]) {
 
 }
 
-void BS(char A[]){
-    return;
+char* BS(char A[]){
+    char sol[50];
+    int i, j;
+    int n = strlen(A);
+
+    for(i = 0; i < n; i++) {
+        if(A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u' || 
+        A[i] == 'A' || A[i] == 'E' || A[i] == 'I'|| A[i] == 'O' || A[i] ==  'U') {
+            
+        } else {}
+    }
+
+
+    return sol;
 }
 
 int main() {
