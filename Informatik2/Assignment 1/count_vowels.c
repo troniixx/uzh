997f0b0792e484f6d6f5c2ae2445d419ebe101b2
merterol <meroert55@gmail.com>
2022-03-02 11:13:14 +0100

    getting close to solving

--- Informatik2/Assignment 1/count_vowels.c
+++ Informatik2/Assignment 1/count_vowels.c
@@ -1,5 +1,26 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+bool is_vowel(int A[]) {
+    int i;
+    int n = sizeof(A);
+
+    for(i = 0; i < n; ++i) {
+        if(A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u' || 
+        A[i] == 'A' || A[i] == 'E' || A[i] == 'I'|| A[i] == 'O' || A[i] ==  'U') {
+            return true;
+        } else {return false;}
+    }
+}
+
+char print_array(char A[]) {
+    int n = sizeof(A);
+
+    for (int i = 0; i < n; ++i){
+        printf("%s", A[i]);
+    }
+}
 
 int count_vowels(char A[]) {
 
@@ -18,20 +39,26 @@ int count_vowels(char A[]) {
 
 }
 
-char* BS(char A[]){
-    char sol[50];
-    int i, j;
-    int n = strlen(A);
-
-    for(i = 0; i < n; i++) {
-        if(A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u' || 
-        A[i] == 'A' || A[i] == 'E' || A[i] == 'I'|| A[i] == 'O' || A[i] ==  'U') {
-            
-        } else {}
+void BS(char A[]){
+    int counter = count_vowels(A);
+    int len_arr = sizeof(A);
+    int len = sizeof(A) + 2*counter;
+    char sol[len+1];
+    int pos = 0;
+    
+    for(int i = 1; i < len_arr; ++i) {
+        if(is_vowel(A[i])){
+            sol[pos] = A[i];
+            sol[pos+1] = 'b';
+            sol[pos+2] = A[i];
+            pos = pos+3;
+        } else {
+            sol[pos] = A[i];
+            pos = pos+1;
+        }
     }
 
-
-    return sol;
+    print_array(sol);
 }
 
 int main() {
