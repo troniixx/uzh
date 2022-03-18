ab913ccc611294bbcc06dfec9be0ea5d9b9ed91f
merterol <meroert55@gmail.com>
2022-03-18 16:35:38 +0100

    Balanced Array algo

---
+++ Informatik2/Algos from Lecture/Recurrances/balanced_array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool is_balanced_iterative(int A[], int l, int r){
+
+    int m = floor((l+r)/2);
+    int size = sizeof(*A)/sizeof(A[0]);
+    int sum1 = 0;
+    int sum2 = 0;
+
+    if(size == 1){ return true;
+    } else {
+        for(int i = 0; i <= m; i++){
+            sum1 = A[i] + sum1;
+        }
+
+        for(m+1; m < size; m++ ){
+            sum2 = A[m] + sum2;
+        }
+
+        if((sum1 <= sum2*2) && (sum1 >= sum2/2)){ return true; } else { return false; }
+
+    }
+
+}
+
+int main(){
+    int arr[5] = {1,2,3,5,7};
+    int arr1[5] = {2,5,3,5,7};
+    printf("If 1 gets returned then the array is balanced: %i\n", is_balanced_iterative(arr, arr[0], arr[4]));
+    printf("If 1 gets returned then the array is balanced: %i", is_balanced_iterative(arr1, arr1[0], arr1[4]));
+}
