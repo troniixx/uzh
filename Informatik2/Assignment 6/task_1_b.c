46cfb7bd93e68b63202cfa89272fbe31c290a785
merterol <meroert55@gmail.com>
2022-04-04 13:17:27 +0200

    started working on ex.6

---
+++ Informatik2/Assignment 6/task_1_b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    double d = 1.2;
+    int i = 6;
+    char ch = 'a';
+
+    double *p_d;
+    int *p_i;
+    char *p_ch;
+
+    p_d = &d;
+    p_i = &i;
+    p_ch = &ch;
+
+    printf("The Values of the variables are %f, %i, %c\n", d, i, ch);
+    printf("The Adresses of the variables are: %x, %x, %x\n", p_d, p_i, p_ch);
+    printf("The Memory Sizes of the variables are: %zu, %zu, %zu\n", sizeof(d), sizeof(i), sizeof(ch));
+}
