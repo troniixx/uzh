9a6bd878c0f0a3b8fa2f382eb2ee5bd965126907
Mert Erol <meroert55@gmail.com>
2023-02-20 17:06:54 +0100

    task3 done

--- Informatik2/FS23/Assignment 0/task3.c
+++ Informatik2/FS23/Assignment 0/task3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int array[10] = {101, 11, 3, 4, 50, 69, 7, 8, 9, 0};
+    int array[5] = {7, 4, 9, 2, 8};
     int loop, largest, second;
 
     //Compare first two elements of the array and define which one is larger (largest) and smaller (second)
@@ -16,7 +16,7 @@ int main()
 
     //work thorugh the array starting at the third element and comapre it to the pre defined largest and second and
     //adjust values of largest and second accordingly
-    for (loop = 2; loop < 10; loop++){
+    for (loop = 2; loop < 5; loop++){
         if (largest < array[loop]){
             second = largest;
             largest = array[loop];
