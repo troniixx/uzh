9b4a665b66cab1fefe49f99d0df496814c1ceb45
Mert Erol <meroert55@gmail.com>
2023-03-21 18:53:37 +0100

    task3 done (quicksort)

--- Informatik2/FS23/Assignment 5/task3.c
+++ Informatik2/FS23/Assignment 5/task3.c
@@ -7,7 +7,7 @@ void swap(int *a, int *b){
     *b = temp;
 }
 
-int partition(int* arr, int left, int right, int pivot_left){
+int partition(int* arr, int left, int right, int* pivot_left){
     if(arr[left] > arr[right]){
         swap(&arr[left], &arr[right]);
     }
@@ -18,6 +18,37 @@ int partition(int* arr, int left, int right, int pivot_left){
 
     int p1 = arr[left]; //left pivot
     int p2 = arr[right]; //right pivot
+
+    while(l <= k){
+
+        //element less than left pivot
+        if(arr[l] < p1){
+            swap(&arr[l], &arr[j]);
+            j++;
+        }
+
+        //element greater equal to right pivot
+        else if(arr[l] >= p2){
+            while(arr[k] > p2 && l < k){
+                k--;
+            }
+            swap(&arr[l], &arr[k]);
+            k--;
+            if(arr[l] < p1){
+                swap(&arr[l], &arr[j]);
+                j++;
+            }
+        }
+        l++;
+    }
+    j--;
+    k++;
+
+    swap(&arr[left], &arr[j]);
+    swap(&arr[right], &arr[k]);
+    *pivot_left = j;
+
+    return k;
 }
 
 void quicksort(int* arr, int left, int right){
@@ -29,4 +60,19 @@ void quicksort(int* arr, int left, int right){
         quicksort(arr, pivot_left+1, pivot_right-1);
         quicksort(arr, pivot_right+1, right);
     }
+}
+
+void printArray(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int arr[] = { 24, 8, 42, 75, 29, 77, 38, 57 };
+    quicksort(arr, 0, 7);
+    printArray(arr, 7);
+    
+    return 0;
 }
