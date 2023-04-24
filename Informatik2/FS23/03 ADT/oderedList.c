fe47a9d517f7970bc2b2f9f25f3244e7b1401aa0
Mert Erol <meroert55@gmail.com>
2023-04-24 15:30:13 +0200

    Ass 7 & cleanup

---
+++ Informatik2/FS23/03 ADT/oderedList.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+
+int main(){
+    
+}
---
+++ Informatik2/FS23/Assignment 7/Task 2/task2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define N 1000
+
+int start = -1;
+int end = -1;
+int queue[N];
+
+void enqueue(); void dequeue(); void printQueue();
+
+int  main(){
+
+}
