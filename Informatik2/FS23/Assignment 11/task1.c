ff2838543ce1434fd3b24dd3240f536d3ad2c605
Mert Erol <meroert55@gmail.com>
2023-05-15 17:12:37 +0200

    Assign. 11 task 1&3 skeleton done

---
+++ Informatik2/FS23/Assignment 11/task1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int longestPath(int x, int y, int m[x][y]);
+
+int main(){
+    return 0;
+}
+
+
+int longestPath(int x, int y, int m[x][y]){
+    return 0;
+}
+
--- Informatik2/FS23/Assignment 11/task3.c
+++ Informatik2/FS23/Assignment 11/task3.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#inlcude <math.h>
+#include <math.h>
 
+void makeHelper(int x, int y, int m[x][y], int top[x][y], int bottom[x][y], int left[x][y], int right[x][y]);
+int biggestPlus(int x, int y, int m[x][y]);
+
+int main(){
+    return 0;
+}
+
+void makeHelper(int x, int y, int m[x][y], int top[x][y], int bottom[x][y], int left[x][y], int right[x][y]){
+    return;
+}
+
+int biggestPlus(int x, int y, int m[x][y]){
+    return 0;
+}
