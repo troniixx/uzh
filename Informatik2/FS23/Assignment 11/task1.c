bc72670dd3b3cb6575d78f61bc2fbb1314fb7f8b
Mert Erol <meroert55@gmail.com>
2023-05-15 17:18:41 +0200

    Assign. 11 task 1&3 skeleton done

--- Informatik2/FS23/Assignment 11/task1.c
+++ Informatik2/FS23/Assignment 11/task1.c
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 
-int longestPath(int x, int y, int m[x][y]);
+int given[4][6] = {{1, 7, 3, 2, 6, 1},
+                    {2, 5, 4, 5, 9, 3}, 
+                    {6, 3, 2, 6, 6, 6}
+                    {8, 7, 5, 4, 8, 7}
+                    };
+
+int longestPath(int x, int y, int m[x][y]); int longestHelper(int x, int y, int m[x][y]);
 
 int main(){
     return 0;
 }
 
+int longestHelper(int x, int y, int m[x][y]){
+    return 0;
+}
 
 int longestPath(int x, int y, int m[x][y]){
     return 0;
--- Informatik2/FS23/Assignment 11/task3.c
+++ Informatik2/FS23/Assignment 11/task3.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
+int given[6][6] = {{0, 0, 0, 1, 0, 0},
+                    {0, 1, 0, 1, 0, 0},
+                    {1, 1, 1, 1, 1, 1},
+                    {1, 1, 0, 1, 0, 1},
+                    {1, 0, 0, 1, 1, 0}
+                    };
+
 void makeHelper(int x, int y, int m[x][y], int top[x][y], int bottom[x][y], int left[x][y], int right[x][y]);
 int biggestPlus(int x, int y, int m[x][y]);
 
