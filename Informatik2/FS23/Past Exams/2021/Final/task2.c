5eac2a808519493f903cd419b7e382f602425a44
Mert Erol <meroert55@gmail.com>
2023-06-19 11:18:47 +0200

    skeleton done

---
+++ Informatik2/FS23/Past Exams/2021/Final/task2.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define m 5;
+
+struct elem{
+    int key;
+    int status // 0; occupied, -1; Empty
+};
+
+struct elem HT[m];
+
+int hash(int k, int i){
+
+}
+
+int HTInsert(int k){
+
+}
+
+int HTDelete(int k){
+
+}
+
+int main(){
+
+}
