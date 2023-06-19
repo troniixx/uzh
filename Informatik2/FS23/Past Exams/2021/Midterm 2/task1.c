e8cc44bc08143d7f170dd85caaf54fc37932cdea
Mert Erol <meroert55@gmail.com>
2023-06-19 11:18:40 +0200

    skeleton done

---
+++ Informatik2/FS23/Past Exams/2021/Midterm 2/task1.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node{
+    int val;
+    struct node* next;
+};
+
+struct node* head;
+
+struct node* rearrange(struct node* h){
+
+}
+
+int main(){
+
+}
