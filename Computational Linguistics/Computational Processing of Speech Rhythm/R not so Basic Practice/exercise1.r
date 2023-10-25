03a2207291f042000cba24ccd66e50d09b673785
Mert Erol <meroert55@gmail.com>
2023-10-25 17:57:42 +0200

    r practice

--- Computational Linguistics/Computational Processing of Speech Rhythm/R not so Basic Practice/exercise1.r
+++ Computational Linguistics/Computational Processing of Speech Rhythm/R not so Basic Practice/exercise1.r
@@ -1,16 +1,20 @@
 # Daten aus der CSV-Datei laden
 data <- read.csv("/Users/merterol/uzh/Computational Linguistics/Computational Processing of Speech Rhythm/R not so Basic Practice/luft.csv", header = TRUE, sep = ",")
+#alle NA Werte entfernen aus der Tabelle
 data <- na.omit(data)
 
 # a) Lineare Regression
 lin_reg <- lm(`ozon` ~ `Lufttemperatur`, data = data)
 
-# Streudiagramm und Regressionsgerade zeichnen
+# Streudiagramm
 plot(data$`Lufttemperatur`, data$`ozon`, main = "Scatter Plot", xlab = "Lufttemperatur", ylab = "Ozon (max)")
+#Regresionsgerade
 abline(lin_reg, col = "red")
 
-# c) Konfidenzintervall
+# c)
+# Mittelwert von Lufttemperatur
 x_bar <- mean(data$`Lufttemperatur`)
+# Konfidenzintervall
 conf_int <- predict(lin_reg, newdata = data.frame(`Lufttemperatur` = x_bar), interval = "confidence")
 
 # d) Vorhersageintervall
@@ -21,7 +25,7 @@ pred_int <- predict(lin_reg, newdata = data.frame(`Lufttemperatur` = x_bar), int
 x_new <- 150
 pred_int_90 <- predict(lin_reg, newdata = data.frame(`Lufttemperatur` = x_new), interval = "prediction", level = 0.90)
 
-
+# Ausgabe der Werte in der Konsole/Terminal
 cat("x_bar = ", x_bar, "\n")
 cat("conf_int = ", conf_int, "\n")
 cat("pred_int = ", pred_int, "\n")
