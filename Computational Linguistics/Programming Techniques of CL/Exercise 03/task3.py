36b3a9b1b0a2839a89028856174d5ee503129718
Mert Erol <meroert55@gmail.com>
2023-11-05 14:15:41 +0100

    pcl 1 ex03 done

--- Computational Linguistics/Programming Techniques of CL/Exercise 03/task3.py
+++ Computational Linguistics/Programming Techniques of CL/Exercise 03/task3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Author(s):
-# date:
+# Author(s): Mert Erol
+# date: 29.10.23
 
 # TASK 3
 
@@ -9,11 +9,61 @@ computercollection = {"Apple II Plus": [1979,"Apple Inc."], "Lisa": [1983,"Apple
 
 # TODO your implementation here
 
-#a
-computercollection["IBM 5150"] = [1981, "IBM"]
-computercollection["Commodore 64"] = [1982, "Commodore International"]
-
 #b
+def oldest_boy(dicti):
+    oldest_year = float('inf')
+    oldest_computer = None
+    for computer, value in dicti.items():
+        if value[0] < oldest_year:
+            oldest_year = value[0]
+            oldest_computer = computer
 
-print(f"I currently have {len(computercollection)} computers in my collection.
-        The oldest is the ")
+    return f"I currently have {len(dicti)} computers in my collection. The oldest is the {oldest_computer} which was released in {oldest_year}.\n"
+
+#c part 1: sort dictionary by company
+def get_company(dicti):
+    return dicti[1][1]
+
+def sorty_boy(dicti):
+    sorted_dicti = dict(sorted(dicti.items(), key = get_company))
+    
+    output = ""
+    
+    for computer, value in sorted_dicti.items():
+        company = value[1]
+        output += f"{company} {computer}\n"
+
+    return output
+
+#c part 2: 
+# return ’The company I own most machines of is [company name]: I own [number] computers by them.’
+def num_machines(dicti):
+    sorted_d = dict(sorted(dicti.items(), key = get_company))
+    
+    counts = {}
+    
+    for company, value in sorted_d.items():
+        company_name = value[1]
+        if company_name in counts:
+            counts[company_name] += 1
+        else:
+            counts[company_name] = 1
+
+    most_owned_company = max(counts, key=counts.get)
+    most_owned_count = counts[most_owned_company]
+
+    return f"The company I own most machines of is {most_owned_company}: I own {most_owned_count} computers by them."
+    
+if __name__ == "__main__":
+    #a
+    computercollection["IBM 5150"] = [1981, "IBM"]
+    computercollection["Commodore 64"] = [1982, "Commodore International"]
+    computercollection["TRS-80"] = [1977, "Tandy Corporation"]
+    computercollection["Macintosh"] = [1984, "Apple Inc."]
+    
+    #b
+    print(oldest_boy(computercollection))
+    
+    #c
+    print(sorty_boy(computercollection))
+    print(num_machines(computercollection))
--- Computational Linguistics/Programming Techniques of CL/Exercise 03/task4.py
+++ Computational Linguistics/Programming Techniques of CL/Exercise 03/task4.py
@@ -1,17 +1,58 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Author(s):
-# date:
+# Author(s): Mert Erol
+# date: 29.10.23
 
 # TASK 4
 
+import sys
 import nltk
 from nltk.corpus import stopwords
 
 stopwords = set(stopwords.words("english"))  # use this set for removing the stopwords
 
 # take a look at the stopwords by printing them (and print the type – so you see it really is a set): 
-print (stopwords)
-print(type(stopwords))
+#print (stopwords)
+#print(type(stopwords))
 
 # TODO your implementation here
+
+with open("/Users/merterol/uzh/Computational Linguistics/Programming Techniques of CL/Exercise 03/dune.txt") as dune:
+    #a
+    dune = dune.read().lower().rsplit()
+    
+    #b
+    def filtering(dune):
+        filtered_dune = []
+        
+        for word in dune:
+            if word not in stopwords:
+                filtered_dune.append(word)
+        
+        return filtered_dune
+
+    #c
+    def frequency(dune):
+        filtered_dune = filtering(dune)
+        frequency = {}
+
+        for word in filtered_dune:
+            if word in frequency:
+                frequency[word] += 1
+            else:
+                frequency[word] = 1
+            
+        return frequency
+
+    #d
+    def get_word(dune):
+        return dune[0]
+    
+    def sorting(dune):
+        freq = frequency(dune)
+        sorted_frequency = sorted(freq.items(), key = get_word)
+        
+        return sorted_frequency
+    
+if __name__ == '__main__':
+    print(sorting(dune))
