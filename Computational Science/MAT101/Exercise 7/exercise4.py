1c20fb094e65efcc284afefecb772f991cf18e7a
Mert Erol <meroert55@gmail.com>
2023-11-14 15:51:46 +0100

    nearly done with mat101ex07

--- Computational Science/MAT101/Exercise 7/exercise4.py
+++ Computational Science/MAT101/Exercise 7/exercise4.py
@@ -1,11 +1,26 @@
 import numpy as np
-import random as r
 
 def monteCarloPi(N: int) -> float:
-    pass
+    points = 0
+    for _ in range(N):
+        x, y = np.random.uniform(-1, 1), np.random.uniform(-1, 1)
+        if x**2 + y**2 <= 1:
+            points += 1
+            
+    return 4*(points/N)
 
 def monteCarloSphere(N: int, d: int) -> float:
-    pass
+    points = 0
+    for _ in range(N):
+        x = np.random.uniform(-1, 1)
+        y = np.random.uniform(-1, 1)
+        z = np.random.uniform(-1, 1)
+        if x**2 + y**2 + z**2 <= 1:
+            points += 1
+    
+    return (2**d)*(points/N)
 
 if __name__ == "__main__":
-    pass
+    #print(monteCarloPi(1000000))
+    print(monteCarloSphere(10000, 100))
+    
