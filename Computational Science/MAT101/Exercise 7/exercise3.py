a1b68dd8ed46c5d4d6620a0070e35b1dce12b4db
Mert Erol <meroert55@gmail.com>
2023-11-14 18:50:23 +0100

    mat101 ex07 done

--- Computational Science/MAT101/Exercise 7/exercise3.py
+++ Computational Science/MAT101/Exercise 7/exercise3.py
@@ -11,9 +11,18 @@ c = a+b
 
 #d
 v = np.array([7, 11, 2022])
-s = np.matmul(b, v)
+d = np.matmul(b, v)
 
 #e
-e = np.array([[1, 0, 4, 4], [0, 1, 4, 4], [0, 0, 19, 0], [0, 0, 0, 2]])
+e = np.array([
+            [1, 0, 4, 4],
+            [0, 1, 4, 4],
+            [0, 0, 19, 0],
+            [0, 0, 0, 2]
+            ])
 
+print(a, "\n")
+print(b, "\n")
+print(c, "\n")
+print(d, "\n")
 print(e)
--- Computational Science/MAT101/Exercise 7/exercise4.py
+++ Computational Science/MAT101/Exercise 7/exercise4.py
@@ -10,17 +10,18 @@ def monteCarloPi(N: int) -> float:
     return 4*(points/N)
 
 def monteCarloSphere(N: int, d: int) -> float:
-    points = 0
-    for _ in range(N):
-        x = np.random.uniform(-1, 1)
-        y = np.random.uniform(-1, 1)
-        z = np.random.uniform(-1, 1)
-        if x**2 + y**2 + z**2 <= 1:
-            points += 1
+    points = np.random.uniform(-1, 1, (N, d))
+    distances = np.sum(points**2, axis=1)
+    points_sphere = np.sum(distances <= 1)
+    volume = 2**d
     
-    return (2**d)*(points/N)
+    return (points_sphere / N) * volume
 
 if __name__ == "__main__":
     #print(monteCarloPi(1000000))
-    print(monteCarloSphere(10000, 100))
-    
+    print(monteCarloSphere(1000000, 10))
+
+"""
+the non spheric way of doing it approximates pi, while the spheric way approximates the volume of a sphere and will only come close
+to pi if the dimension is low enough and N is high enough (close to infinity)
+"""
