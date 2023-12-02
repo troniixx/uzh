5f431a719f0b2f23c300dc94d3153be417f6c717
Mert Erol <meroert55@gmail.com>
2023-12-02 16:22:36 +0100

    naming scheme changes

--- Computational Linguistics/Programming Techniques of CL/Course Project/grep.sh
+++ Computational Linguistics/Programming Techniques of CL/Course Project/grep.sh
@@ -1,12 +1,12 @@
 # Directory where chapter files are located
 #chapter_dir="/Users/merterol/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Alice/Chapters"
-chapter_dir="/Users/merterol/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Dracula/Chapters"
 #chapter_dir="/Users/merterol/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Dracula/Chapters"
+chapter_dir="/Users/merterol/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Dracula/Chapters"
 
 # Directory where you want to save the analysis results
 #results_dir="/Users/merterol/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Alice/Results"
-results_dir="/Users/merterol/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Dracula/Results"
-#results_dir="/Users/merterol/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Frankenstein/Results"
+#results_dir="/Users/merterol/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Dracula/Results"
+results_dir="/Users/merterol/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Frankenstein/Results"
 
 # Create the results directory if it doesn't exist
 mkdir -p "$results_dir"
@@ -15,7 +15,7 @@ for file in "$chapter_dir"/*; do
     echo "Analyzing $file..."
 
     # Extract filename without path for naming the results file
-    filename=$(basename "$file")
+    filename=$(basename "$file" .txt)
     
     # Define the output file for the results
     output_file="$results_dir/${filename}_analysis.txt"
@@ -26,7 +26,7 @@ for file in "$chapter_dir"/*; do
     
     # Grep for basic sentiment words
     echo -e "\nSentiment Expressions:" >> "$output_file"
-    grep -owiE 'happy|joy|sorrow|sad|love|hate|excited|angry|afraid|peaceful' "$file" | sort | uniq -c | sort -nr >> "$output_file"
+    grep -owiE 'happy|joy|sorrow|sad|love|hate|excited|angry|afraid|peaceful|regret|doubt|adore|depressed|' "$file" | sort | uniq -c | sort -nr >> "$output_file"
 done
 
 echo "Analysis complete. Results saved in $results_dir"
