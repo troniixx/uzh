c828f4b2f35ed5232d496d98e36e7f77e3141d0f
Mert Erol <meroert55@gmail.com>
2023-12-10 13:31:33 +0100

    finished mock1 mat101

--- Computational Science/MAT101/Mock Exam 1/Exercise_2.py
+++ Computational Science/MAT101/Mock Exam 1/Exercise_2.py
@@ -5,6 +5,7 @@ import numpy as np
 from numpy.typing import NDArray
 import matplotlib.pyplot as plt
 from time import perf_counter_ns as timer
+import math
 
 # a)
 def partial_sum(n: int) -> float:
@@ -19,16 +20,26 @@ def partial_sum(n: int) -> float:
 
 # b)
 def convergence_vector(v: list) -> list:
+    if not all(isinstance(num, int) and num >= 0 for num in v):  # Ensure v is a list of natural numbers
+        raise ValueError("Error: all elements of the list should be natural numbers")
+    
     return [partial_sum(n) for n in v]
 
+# c)
 def convergence_vector_opt(v: list) -> list:
     if not all(isinstance(num, int) and num >= 0 for num in v):  # Ensure v is a list of natural numbers
         raise ValueError("Error: all elements of the list should be natural numbers")
 
     max_val = max(v)
-    
-    
-    
+    partial_sums = [0] * (max_val + 1)
+
+    res, sign = 0, 1
+    for num in range(1, max_val + 2):
+        res += sign / num
+        partial_sums[num - 1] = res
+        sign *= -1
+        
+    return [partial_sums[n] for n in v]
 
 def time_comparison(v: list) -> None:
     time_conv = timer()
@@ -45,6 +56,24 @@ def time_comparison(v: list) -> None:
     print("Result for convergence_vector_opt: ", result_conv_opt)
     
     return None
+
+def plotter(v: list) -> None:
+    res_opt = convergence_vector_opt(v)
+    
+    plt.figure()
+    plt.scatter(v, res_opt, label = "Partial Sums")
+    plt.xscale("log")
+    plt.axhline(math.log(2), color='r', linestyle='dashed', label='log(2)')
+    plt.title("Partial Sums vs log(2)")
+    plt.xlabel("n")
+    plt.legend()
+    plt.grid(True)
+    
+    plt.savefig("/Users/merterol/uzh/Computational Science/MAT101/Mock Exam 1/plot_convergence.pdf")
+    #plt.show()
+
 if __name__ == "__main__":
     v = [100, 200]
+    n = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
     time_comparison(v)
+    plotter(n)
--- Computational Science/MAT101/Mock Exam 1/Exercise_3.py
+++ Computational Science/MAT101/Mock Exam 1/Exercise_3.py
@@ -5,31 +5,68 @@ import numpy as np
 from numpy.typing import NDArray
 import matplotlib.pyplot as plt
 
-def degree(P: list, x: int) -> float:
-    pass
+def degree(P: list) -> float:
+    return len(P)-1
 
 def eval_polynomial(P: list, x: float) -> float:
-    pass
+    return sum(coef * (x**i) for i, coef in enumerate(P))
 
-def polynominal_derivative(P: list) -> list:
-    pass
+def polynomial_derivative(P: list) -> list:
+    return [coef * (i + 1) for i, coef in enumerate(P[1:])]
 
 class Polynomial:
+    def __init__(self, coeffs):
+        self.coeffs = coeffs
+        self.degree = degree(coeffs)
     
-    def __init__(self, d: int, coefs: list):
-        self.d = d
-        self.coefs = coefs
-        
-    def derivative(self, d, coefs):
-        if d < 1:
-            return [1]
-        
-        res = []
-        
-        for coef in self.coefs:
-            res.append(coef-1)
-            
+    def derivative(self):
+        derived_coeffs = polynomial_derivative(self.coeffs)
+        return Polynomial(derived_coeffs)
+    
     def __add__(self, other):
-        res = []
-        for idx, coef in enumerate(self.coefs):
-            res.append(coef + other.coefs[idx])
+        # find the length of the longer polynomial
+        max_length = max(len(self.coeffs), len(other.coeffs))
+        
+        # extend both coefficient lists to the length of the longer one, using 0 for missing coefficients
+        coeffs1 = self.coeffs + [0] * (max_length - len(self.coeffs))
+        coeffs2 = other.coeffs + [0] * (max_length - len(other.coeffs))
+        
+        # sum the coefficients
+        new_coeffs = [a + b for a, b in zip(coeffs1, coeffs2)]
+        
+        # remove trailing zeros (if any) to avoid incorrect degree
+        while new_coeffs and new_coeffs[-1] == 0:
+            new_coeffs.pop()
+        
+        return Polynomial(new_coeffs)
+
+    # __str__ method to print the polynomial in a nice way, without this i just the the address of the object in memory
+    def __str__(self):
+        terms = [f"{coef}x^{i}" for i, coef in enumerate(self.coeffs) if coef]
+        return " + ".join(terms).replace("x^0", "").replace("x^1", "x").replace(" + -", " - ").strip()
+
+
+if __name__ == "__main__":
+    P_one = [1, 0, 2]
+    P_two = [2, -6, 2, -1]
+    P_three = [0, 0, 0, 5]
+    x_value = 4
+    
+    # a) degree of the polynomial
+    print("Degree of the polynomial:", degree(P_three))
+
+    # b) evaluation of the polynomial at x_value
+    print("P({}) =".format(x_value), eval_polynomial(P_three, x_value))
+
+    # c) derivative of the polynomial
+    print("Derivative coefficients:", polynomial_derivative(P_three))
+
+    # d) using the class Polynomial
+    poly = Polynomial(P_three)
+    poly_derivative = poly.derivative()
+    print("Polynomial:", poly)
+    print("Derivative Polynomial:", poly_derivative)
+
+    # addition of two polynomials
+    poly_sum = poly + poly_derivative
+    print("Sum of Polynomial and its Derivative:", poly_sum)
