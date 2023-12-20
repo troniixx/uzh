d9c791297efc44c0d148a336cb318ec540641b37
troniixx <meroert55@gmail.com>
2023-12-20 11:42:18 +0100

    finished extract_entity_info PCL Project

--- Computational Linguistics/Programming Techniques of CL/Course Project/Part1.py
+++ Computational Linguistics/Programming Techniques of CL/Course Project/Part1.py
@@ -11,24 +11,24 @@ import spacy
 import nltk
 # --- You may add other imports here ---
 import tqdm
+from sys import argv
 
+# TODO: add argv stuff
 # ****** IMPORTANT: Change paths to fit your system ******
-PATH_ALICE = "Computational Linguistics/Programming Techniques of CL/Course Project/Alice/alice_cleaned.txt"
-PATH_DRACULA = "Computational Linguistics/Programming Techniques of CL/Course Project/Dracula/dracula_cleaned.txt"
-PATH_FRANKY = "Computational Linguistics/Programming Techniques of CL/Course Project/Frankenstein/franky_cleaned.txt"
+PATH_ALICE = "Computational Linguistics/Programming Techniques of CL/Course Project/Alice/Chapters"
+PATH_DRACULA = "Computational Linguistics/Programming Techniques of CL/Course Project/Dracula/Chapters"
+PATH_FRANKY = "Computational Linguistics/Programming Techniques of CL/Course Project/Frankenstein/Chapters"
 
-PATH_ALICE_NMSG = "/Users/merterol/Desktop/VSCode/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Alice/alice_cleaned.txt"
-PATH_DRACULA_NMSG = "/Users/merterol/Desktop/VSCode/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Dracula/dracula_cleaned.txt"
-PATH_FRANKY_NMSG = "/Users/merterol/Desktop/VSCode/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Franky/franky_cleaned.txt"
+PATH_ALICE_NMSG = "/Users/merterol/Desktop/VSCode/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Alice/Chapters"
+PATH_DRACULA_NMSG = "/Users/merterol/Desktop/VSCode/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Dracula/Chapters"
+PATH_FRANKY_NMSG = "/Users/merterol/Desktop/VSCode/uzh/Computational Linguistics/Programming Techniques of CL/Course Project/Franky/Chapters"
 # ****** IMPORTANT: Change paths to fit your system ******
 
 # DONE: Load the spaCy model
 SPACY_MODEL = spacy.load("en_core_web_sm")
 
-# set of gutenberg strings that would be considered as named entities and were not filtered by gutenberg_cleanup.py 
-# these are not relevant to our analysis
+# set of names we used for main character analysis
 # i used a set because it has faster lookup times than a normal list
-GUTENBERG_BS = set(["Gutenberg", "Project Gutenberg", "Project\nGutenberg", "LIMITED WARANTY", "LIMITED WARRANTY", "Gutenberg eBooks", "Michael S. Hart"])
 LIST_NAMES = set(["Alice", "Queen", "King", "Gryphon", "Hatter", "Mock Turtle", "Duchess", "Dormouse", "Mouse", "Rabbit", "Elizabeth", "Clerval",
             "Justine", "Felix", "Victor", "Safie", "Henry", "William", "Agatha", "Kirwin", "Van Helsing", 
             "Lucy", "Jonathan", "Count", "Arthur","Seward", "Mina", "Quincey", "Renfield"])
@@ -43,72 +43,83 @@ def load_books_by_chapter(folder_path):
                 chapters.append(file.read())
     return chapters
 
-# Feel free to add more functions as needed!
-def rem_puc(text):
-    text = re.sub(r"[^\w\s]")
-    text = re.sub(r'\n', ' ', text)  # Replace newlines with space
-    return text
-
-
-
-# Function to process the text and perform NER
+# function to process the text and perform NER
 def perform_ner(text, spacy_model):
-    # TODO: Process the text using the provided model and return the entities
+    # DONE: Process the text using the provided model and return the entities
 
     ent_list = []
     doc = spacy_model(text)
 
     for ent in doc.ents:
-        if ent.label_ == "PERSON" and ent.text not in GUTENBERG_BS and "\n" not in ent.text:
+        if ent.label_ == "PERSON" and "\n" not in ent.text and ent.text in LIST_NAMES:
             ent_list.append(ent.text)
 
     return ent_list
 
-def get_chapter_number(sentence):
-    
+# function to remove punctuation and newlines (got the regex from chatgpt)
+def rem_puc(text):
+    text = re.sub(r'[!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]', '', text) # remove punctuation
+    text = re.sub(r'\n', ' ', text) # replace newlines with space
+    return text
 
-# TODO: Function to extract and structure entity information
-def extract_entity_info(text):
-    # Clean the text
-    text_clean = rem_puc(text)
-    doc = SPACY_MODEL(text_clean)
+# function to extract chapter number from filename (got this from chatgpt)
+def extract_chapter_number(filename):
+    # extract digits from filename and convert to int
+    chapter_num = int(re.search(r'\d+', filename).group())
+    return chapter_num
 
-    # Initialize data structure
-    main_characters = []
+# Function to extract and structure entity information from chapters
+def extract_entity_info(folder_path, spacy_model, character_list):
+    # initialize data structure
+    all_chapters_info = {"main_characters": []}
 
-    # Iterate over the list of character names
-    for character in LIST_NAMES:
-        character_info = {
-            "name": character,
-            "aliases": [],  # Add aliases if you have them
-            "occurrences": []
-        }
+    # load the spaCy model
+    nlp = spacy_model
 
-        # Iterate over the entities in the document
-        for ent in doc.ents:
-            if ent.text == character:
-                start_idx = ent.start_char
-                end_idx = ent.end_char
-                sentence = ent.sent.text if ent.sent else "No sentence found"
-                chapter_number = get_chapter_number(ent.sent)  # TODO implement this function
+    # iterate over sorted chapter files
+    for filename in sorted(os.listdir(folder_path)):
+        if filename.endswith('.txt'):
+            chapter_path = os.path.join(folder_path, filename)
+            with open(chapter_path, 'r', encoding='utf-8') as file:
+                chapter_text = file.read()
 
-                occurrence = {
-                    "sentence": sentence,
-                    "chapter": chapter_number,
-                    "position": {"start": start_idx, "end": end_idx}
+            # clean the chapter text
+            text_clean = rem_puc(chapter_text)
+            doc = nlp(text_clean)
+
+            # extract chapter number from filename
+            chapter_number = extract_chapter_number(filename)
+
+            # iterate over the list of character names
+            for character in character_list:
+                character_info = {
+                    "name": character,
+                    "aliases": [],  # TODO: add aliases here
+                    "occurrences": []
                 }
 
-                character_info["occurrences"].append(occurrence)
+                # iterate over the entities in the document
+                for ent in doc.ents:
+                    if ent.text == character:
+                        start_idx = ent.start_char
+                        end_idx = ent.end_char
+                        sentence = ent.sent.text if ent.sent else "No sentence found"
 
-        if character_info["occurrences"]:
-            main_characters.append(character_info)
+                        occurrence = {
+                            "sentence": sentence,
+                            "chapter": chapter_number,
+                            "position": {"start": start_idx, "end": end_idx}
+                        }
 
-    return {"main_characters": main_characters}
+                        character_info["occurrences"].append(occurrence)
+
+                if character_info["occurrences"]:
+                    all_chapters_info["main_characters"].append(character_info)
+
+    return all_chapters_info
 
 
-
-
-# TODO: Function to save data to JSON file
+# TODO: function to save data to JSON file
 def save_to_json(data, filename):
     """ should look something like this:
 
@@ -140,8 +151,6 @@ def save_to_json(data, filename):
 def main():
 
     # TODO: Load your book text here
-    
-    book_text = load_book(PATH_ALICE)
 
     # Perform NER on the text
     #print("Entities")
@@ -149,7 +158,7 @@ def main():
     #print(entities)
     # Extract information from entities
     print("Entity info:")
-    entity_info = extract_entity_info(book_text)
+    entity_info = extract_entity_info(PATH_ALICE_NMSG, SPACY_MODEL, LIST_NAMES)
     print(entity_info)
     
     # Save the results to a JSON file
