ae5b3a95f100b2c7fc0809df8077ea6a598f0889
Mert Erol <meroert55@gmail.com>
2024-02-21 16:52:32 +0100

    sta120 ex1

--- Computational Science/STA120/Access/Exercise 01/task1.r
+++ Computational Science/STA120/Access/Exercise 01/task1.r
@@ -1,7 +1,11 @@
 png(file = "solution.png")
-require( ... ) # Or library(fields) 
-... ( ... ) # Check out the plot in your R-Studio
+require(fields) # Or library(fields)
+
+image.plot(volcano)
 # function which.max() returns you the heighest value of your dataset (there is also which.min())
 
-max_height <- ... 
+max_height <- which.max(volcano)
+image.plot(volcano)
 dev.off()
+
+print(max_height)
