43a4ed3caaa623db6a3218533320b049e30a15aa
Mert Erol <71695384+troniixx@users.noreply.github.com>
2024-02-24 11:02:42 +0100

    finished STA120 ex. 1

--- Computational Science/STA120/Access/Exercise 01/task3.r
+++ Computational Science/STA120/Access/Exercise 01/task3.r
@@ -1,11 +1,8 @@
 png(file="solution.png")
-dat <- read.csv("Computational Science/STA120/Access/Exercise 01/wolvesmoose.csv", header=TRUE)
-
+dat <- read.csv("resource/wolvesmoose.csv", header=TRUE)
 par(mfrow = c(2,3))
 
-boxplot(dat$Wolf, dat$Moose, 
-        names = c("Wolf Abundance", "Moose Abundance"), 
-        main="Boxplot of Moose and Wolves")
+boxplot(dat$Moose, dat$Wolf)
 
 qqnorm(dat$Moose, main="QQ-plot Moose")
 qqline(dat$Moose)
@@ -13,16 +10,16 @@ qqline(dat$Moose)
 qqnorm(dat$Wolf, main="QQ-plot of Wolves")
 qqline(dat$Wolf)
 
-plot(dat$Year, dat$Wolf, type = "l", main="Wolf Abundance over Year", xlab = "Year", ylab = "Wolf Abundance")
-plot(dat$Year, dat$Moose,  type = "l", main="Moose Abundance over Year", xlab = "Year", ylab = "Moose Abundance")
+plot(Wolf ~ Year, data = dat, type = "l")
+plot(Moose ~ Year, data = dat,  type = "l")
 
 # How many times should the colors repeat so that the legend makes sense?
-plot(dat$Moose, dat$Wolf, col=rep( c(1,2,3), times=c(20, 25, 30)))
+plot(Wolf ~ Moose, data = dat, col=rep( c(1,2,3), times=c(1, 1, 1)))
 
 
-legend("topright", col=c(1,3,4), 
+legend("topright", col=c(1,2,3), 
     legend=c("1959-1980","1981-1996","1997-2011"), 
     bty="n", 
     pch="o")
 
-null <- dev.off()
+dev.off()
