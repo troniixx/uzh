0ad631d9b9474e7b73cd6f54bc5115df160b2ad5
Mert Erol <71695384+troniixx@users.noreply.github.com>
2024-03-03 11:46:07 +0100

    finished exercise2 STA120

--- Computational Science/STA120/Access/Exercise 02/task2.r
+++ Computational Science/STA120/Access/Exercise 02/task2.r
@@ -1,6 +1,6 @@
 png(file="solution.png")
-P1 <- pnorm(4, 2, sqrt(16))
-P2 <- pnorm(4, 2, sqrt(4)) - pnorm(0, 2, sqrt(16))
+P1 <- pnorm(4, mean = 2, sd = sqrt(16))
+P2 <- pnorm(4, mean = 2, sd = sqrt(16)) - pnorm(0, mean = 2, sd = sqrt(16))
 Q1 <- qnorm(0.95, 2, sqrt(16), lower.tail = FALSE)
 Q2 <- -qnorm(0.05, 2, sqrt(16))
 
