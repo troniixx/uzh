2863df2215b9f84fe18c78a1122ba85f75d27811
Mert Erol <meroert55@gmail.com>
2024-03-08 14:40:47 +0100

    pcl tutorial ex

--- Computational Linguistics/Sem 2/PCL 2/Tutorial/Tutorial 3/ex1and2.py
+++ Computational Linguistics/Sem 2/PCL 2/Tutorial/Tutorial 3/ex1and2.py
@@ -9,25 +9,27 @@ class BTP():
         return len(self.text.split())
     
 class ATP(BTP):
-        def __init__(self, str):
-            super().__init__(str)
+    def __init__(self, str, sw):
+        super().__init__(str)
+        self.sw = sw
         
-        def wc(self):
-            sw = ["the", "is", "at", "which", "on"]
-            return  len([x for x in self.text.split() if x not in sw])
-
-        def unique_word(self):
-            return len(set(self.text.split()))
-            
+    def wc(self):
+        return  len([x for x in self.text.split() if x not in self.sw])
+    
+    def unique_word(self):
+        return len(set(self.text.split()))
+        
 
 if __name__ == "__main__":
+    sw = ["the", "is", "at", "which", "on"]
+    
     print("BTP")
     btp = BTP("This is a test string This is a test string")
     print(btp.lc_text())
     print(btp.wc())
     
     print("\nATP")
-    atp = ATP("This is a test string This is a test string")
+    atp = ATP("This is a test string This is a test string", sw)
     print(atp.lc_text())
     print(atp.wc())
     print(atp.unique_word())
