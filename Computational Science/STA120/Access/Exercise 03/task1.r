380b8b0516ef504b4d6a1b0fcc35eebfee4ec893
Mert Erol <71695384+troniixx@users.noreply.github.com>
2024-03-10 15:29:19 +0100

    Update task1.r

--- Computational Science/STA120/Access/Exercise 03/task1.r
+++ Computational Science/STA120/Access/Exercise 03/task1.r
@@ -1,20 +1,23 @@
 png(file="solution.png")
-    set.seed(3)
-    lambda1 <- 0.2
-    lambda2 <- 2
-    n <- 200
-    
-    data1 <- ...
-    data2 <- ...
-    
-    # Pay attention to the binning to be used for count data!
-    par(mfrow = c(1, 2))
-    hist( ... , xlab = "", prob = TRUE,
-        breaks = seq(min(data1)-0.5, max(data1)+0.5, by = 1),
-        main = expression(lambda == 0.2))
-    points( ... , ... (unique( ... ), ... ), pch = 19)
-    hist(data2, xlab = "", prob = TRUE,
-        breaks = seq(min(data2)-0.5, max(data2)+0.5, by = 1),
-        main = expression(lambda == 2))
-    points( ... , ... (unique( ... ), lambda2), pch = 19)
+  set.seed(3)
+  lambda1 <- 0.2
+  lambda2 <- 2
+  n <- 200
+  
+  data1 <- rpois(n, lambda1)
+  data2 <- rpois(n, lambda2)
+  
+  # Pay attention to the binning to be used for count data!
+  par(mfrow = c(1, 2))
+  hist(data1, xlab = "", prob = TRUE,
+       breaks = seq(min(data1)-0.5, max(data1)+0.5, by = 1),
+       main = expression(lambda == 0.2))
+  # Use the points funtion to overlay the theoretical values of the PMF
+  points(unique(data1) , dpois(unique(data1), lambda1), pch = 19)
+
+  hist(data2, xlab = "", prob = TRUE,
+       breaks = seq(min(data2)-0.5, max(data2)+0.5, by = 1),
+       main = expression(lambda == 2))
+  points(unique(data2) , dpois(unique(data2), lambda2), pch = 19)
+
 dev.off()
