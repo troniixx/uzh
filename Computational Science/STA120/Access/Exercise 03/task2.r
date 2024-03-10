09c3e12ffdce4751f5ca2d0a7821af6d4c840610
Mert Erol <71695384+troniixx@users.noreply.github.com>
2024-03-10 15:30:03 +0100

    Update task2.r

--- Computational Science/STA120/Access/Exercise 03/task2.r
+++ Computational Science/STA120/Access/Exercise 03/task2.r
@@ -1,15 +1,19 @@
 png(file="solution.png")
-  lambda <- ...
+  lambda <- 5
   grid <- 0:12
-  n <- ...
+  n <- c(10, 100, 1000)
   
   par(mfrow = c(1, 3))
   for (i in 1:length(n)) {
-    plot( ... , ... , main = paste("n =", n[i]), type = "h",
+    plot(grid , dpois(grid, lambda) , main = paste("n =", n[i]), type = "h",
          ylim = c(0, 0.25), xlab = "", ylab = "", col = 1)
-    points( ... , ..., col = 1, pch = 20)
-    points( ... +0.1, ... , type = "h", col = 2)
-    points((grid)+0.1, ... , col = 2, pch = 20)
+
+    points( grid, dpois(grid, lambda), col = 1, pch = 20)
+
+    points( (grid) +0.1, dbinom(grid, size = n[i], p = lambda/n[i]) , type = "h", col = 2)
+
+    points((grid)+0.1, dbinom(grid, size = n[i], p = lambda/n[i]) , col = 2, pch = 20)
+
     legend("topright", pch = 20, legend = c("Pois", "Binom"), col = c(1, 2))
   }
 dev.off()
