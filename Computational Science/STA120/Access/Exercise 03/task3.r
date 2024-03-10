62075f5065847495f2710babaafc06e25bc99209
Mert Erol <71695384+troniixx@users.noreply.github.com>
2024-03-10 15:30:22 +0100

    Update task3.r

--- Computational Science/STA120/Access/Exercise 03/task3.r
+++ Computational Science/STA120/Access/Exercise 03/task3.r
@@ -1,11 +1,11 @@
 png(file="solution.png")
 set.seed(12)
-n <- ...
+n <- 100
 
 # Sample the data
-hist( ... , probability = TRUE, main = "") # Histogram of the sampled data with a superimposed theoretical density. 
+hist( rexp(n, rate = 2) , probability = TRUE, main = "") # Histogram of the sampled data with a superimposed theoretical density. 
 
 # Using curve() function plot the theroretical function. For more info: ?curve()
-...
+curve(dexp(x, rate = 2), add = TRUE)
 
 dev.off()
