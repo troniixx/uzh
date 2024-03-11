e812529a5d85c426713e291dd175fc5f758fda6a
Mert Erol <71695384+troniixx@users.noreply.github.com>
2024-03-11 19:49:32 +0100

    Update task5.r

--- Computational Science/STA120/Access/Exercise 03/task5.r
+++ Computational Science/STA120/Access/Exercise 03/task5.r
@@ -1,25 +1,25 @@
 png(file="solution.png")
-  lambda1 <- ...
-  lambda2 <- ...
+  lambda1 <- 0.2
+  lambda2 <- 2
   grid <- 0:8
   
   par(mfrow = c(1, 2))
-  plot( ... , dpois( ... , ... ), main = "PMFs", type = "h",
+  plot(grid , dpois(grid , lambda1 ), main = "PMFs", type = "h",
        xlab = expression(x), ylab = expression(f(x)), col = "blue")
-  points( ... , dpois( ... , ... ), col = "blue", pch = 20)
-  points( ... +0.1, ... , type = "h", col = "red")
-  points( ... +0.1, ... , col = "red", pch = 20)
+  points(grid, dpois( grid , lambda1 ), col = "blue", pch = 20)
+  points( grid +0.1, dpois(grid, lambda2) , type = "h", col = "red")
+  points( grid+0.1, dpois(grid, lambda2) , col = "red", pch = 20)
   legend("topright", pch = 20,
          legend = c(expression(lambda == 0.2), expression(lambda == 2)), 
          col = c("blue", "red"))
   
-  probs1 <- ... # Use ppois() here, and make sure to include an additional point at 0
-  probs2 <- ... # Use ppois() here
+  probs1 <- c(0, ppois(grid, lambda1)) # Use ppois() here, and make sure to include an additional point at 0
+  probs2 <- c(0, ppois(grid, lambda2)) # Use ppois() here
   
-  plot(stepfun( ... ), verticals = ... , pch = 20, 
+  plot(stepfun( grid, probs1 ), verticals = TRUE , pch = 20, 
        xlab = expression(x), ylab = expression(F(x)), 
        main = "CDFs", col = "blue")
-  plot( ... , verticals = ..., add = TRUE, pch = 20, col = "red")
+  plot( stepfun(grid, probs2) , verticals = TRUE, add = TRUE, pch = 20, col = "red")
   legend("bottomright", pch = 20, 
          legend = c(expression(lambda == 0.2), expression(lambda == 2)),
          col = c("blue", "red"))
