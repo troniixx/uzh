de318f2a813137b9cc95e433d7998383af4248f1
troniixx <meroert55@gmail.com>
2024-03-14 11:22:44 +0100

    working on STA120 ex4

--- Computational Science/STA120/Access/Exercise 04/task3.r
+++ Computational Science/STA120/Access/Exercise 04/task3.r
@@ -3,10 +3,11 @@ HbSS <- c(7.2, 7.7, 8, 8.1, 8.3, 8.4, 8.4, 8.5, 8.6, 8.7, 9.1,
 HbSb <- c(8.1, 9.2, 10, 10.4, 10.6, 10.9, 11.1, 11.9, 12.0, 12.1)
 
 # CI for HbSS
-n_SS <- ...
-CI_HbSS <-  ... + c(-1, 1) * ... * ...
+n_SS <- length(HbSS)
+CI_HbSS <- mean(HbSS) + c(-1, 1) * qnorm(0.95) * 1/sqrt(n_SS)
+
 # CI for HbSb
-n_Sb <- ...
-CI_HbSb <- ... + c(-1, 1) * ... * ...
+n_Sb <- length(HbSb)
+CI_HbSb <- mean(HbSb) + c(-1, 1) * qnorm(0.95) * 1/sqrt(n_Sb)
 
 sol <- list(CI_HbSS, CI_HbSb)
--- Computational Science/STA120/Access/Exercise 04/task4.r
+++ Computational Science/STA120/Access/Exercise 04/task4.r
@@ -1,15 +1,15 @@
-require( ... )
-data( ... )
+require( spam )
+data( Oral )
+str(Oral)
 
+smry <- summary(Oral)
+maximum <- max(Oral)
 
-smry <- ...
-maximum <- ...
-
-mean_SMR <- ...
+mean_SMR <- mean(Oral$SMR)
 n <- 544 
-S2 <- ...
-S <- ...
-ci <- ... + c(-1, 1) * ... * ...
+S2 <- sum((Oral$SMR - mean_SMR)^2)/(n-1)
+S <- sqrt(S2)
+ci <- mean(Oral$SMR) + c(-1, 1) * qt(0.975, n-1, lower = TRUE) * S/sqrt(n)
 
 
 sol <- list(smry, maximum, mean_SMR, ci)
