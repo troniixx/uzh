e0b439299d4ee66ae90f5b2d2155a63138aa6c11
Mert Erol <meroert55@gmail.com>
2024-03-19 21:15:41 +0100

    reorg

--- Computational Science/00 Test/x.py
+++
@@ -1,6 +0,0 @@
-sentence = "You are using Python right now." #subtask a
-print(f"Subtask b: {type(sentence)}") 
-print(f"Subtask c: {sentence[0]}")
-print(f"Subtask d: {sentence[-7:]}")
-print(f"Subtask e: {sentence[0:3]}")
-print(f"Subtask f: {len(sentence)}")
