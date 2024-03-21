0940393847b2ec2f5a2ee3cdfc9577ae84fa83d2
troniixx <meroert55@gmail.com>
2024-03-21 13:54:03 +0100

    finished sta120 assignment 5

--- Computational Science/Sem 2/STA120/Access/Exercise 05/task5.r
+++ Computational Science/Sem 2/STA120/Access/Exercise 05/task5.r
@@ -1,9 +1,16 @@
 set.seed(5)
 
-...
+pvals = c()
+
+for (i in 1:1000) {
+    ttest = t.test(rnorm(10, mean = 0, sd = 1), conf.level = 0.95)
+    pvals = c(pvals, ttest$p.value)
+}
 
 hist(pvals, main = "Histogram of p-values", xlab = "p-values")
 
-...
+err = sum(pvals < 0.05) / 1000
 
 sol <- list(type_I_error = err)
+
+print(sol)
--- Computational Science/Sem 2/STA120/Access/Exercise 05/task6.r
+++ Computational Science/Sem 2/STA120/Access/Exercise 05/task6.r
@@ -1,17 +1,25 @@
 set.seed(5)
-...
-# t-distribution
-...
+pvals_t = c()
+pvals_chi2 = c()
 
-typeI_t <-...
+# t-distribution
+for (i in 1:1000) {
+    ttest = t.test(rnorm(10, mean = 0, sd = 1), conf.level = 0.95)
+    pvals_t = c(pvals_t, ttest$p.value)
+}
+typeI_t <- sum(pvals_t < 0.05) / 1000
 
 #chi-squared distribution
-...
-typeI_chi2 <- ...
+for(i in 1:1000){
+    obs <- table(factor(sample(1:2, 10, replace = TRUE), levels = 1:2))
+    exp <- c(5, 5)
+    chi2test = chisq.test(obs, p = exp/sum(exp))
+    pvals_chi2 = c(pvals_chi2, chi2test$p.value)
+}
+typeI_chi2 <- sum(pvals_chi2 < 0.05) / 1000
 
 par(mfrow = c(1, 2))
-hist(..., main = "t-distribution", xlab = "p-values")
-hist(..., main = "chi-square distribution", xlab = "p-values")
-
+hist(pvals_t, main = "t-distribution", xlab = "p-values")
+hist(pvals_chi2, main = "chi-square distribution", xlab = "p-values")
 
 sol <- list(typeI_t = typeI_t, typeI_chi2=typeI_chi2)
