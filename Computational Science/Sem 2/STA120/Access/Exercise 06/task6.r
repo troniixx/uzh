b61759d3dca30dbf7db9ab8e42e75af8da99a147
Mert Erol <meroert55@gmail.com>
2024-04-05 11:01:44 +0200

    working on STA120 (t3 and t6 open)

--- Computational Science/Sem 2/STA120/Access/Exercise 06/task6.r
+++ Computational Science/Sem 2/STA120/Access/Exercise 06/task6.r
@@ -1,20 +1,23 @@
 ORCI <- function(x1, x2, n1, n2, alpha = 0.05) {
-    
-    ...
+    p1 <- x1 / n1
+    p2 <- x2 / n2
+    z <- qnorm(1 - alpha / 2)
     
     CI <- exp( ... + c(-1, 1) * ... * ...)
     return(round(CI, 4))
 }
 
 RRCI <- function(x1, x2, n1, n2, alpha = 0.05) {
-
-    ... 
+    p1 <- x1 / n1
+    p2 <- x2 / n2
+    rr <- p1 / p2
+    z <- qnorm(1 - alpha / 2)
     
     CI <- exp( ... + c(-1, 1) * ... * ...)
     return(round(CI , 4))
 }
 
-OR_CI <- ORCI(x1 = ... , x2 = ... , n1 = ... , n2 = ... )
-RR_CI <- RRCI(x1 = ... , x2 = ... , n1 = ... , n2 = ... )
+OR_CI <- ORCI(x1 = 9 , x2 = 5 , n1 = 27 , n2 = 27 )
+RR_CI <- RRCI(x1 = 9 , x2 = 5 , n1 = 27 , n2 = 27 )
 
 sol <- list(OR_CI = OR_CI, RR_CI = RR_CI)
