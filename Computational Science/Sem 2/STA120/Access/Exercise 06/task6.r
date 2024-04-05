d663fecdd0ecbc4260d2420943bee9a9a240d2c4
Mert Erol <meroert55@gmail.com>
2024-04-05 11:10:40 +0200

    STA120 t3 and t6 .5/1 rrest done

--- Computational Science/Sem 2/STA120/Access/Exercise 06/task6.r
+++ Computational Science/Sem 2/STA120/Access/Exercise 06/task6.r
@@ -2,8 +2,10 @@ ORCI <- function(x1, x2, n1, n2, alpha = 0.05) {
     p1 <- x1 / n1
     p2 <- x2 / n2
     z <- qnorm(1 - alpha / 2)
+    or <- (p1 / (1 - p1)) / (p2 / (1 - p2))
+    log_or <- sqrt(1/x1 - 1/n1 + 1/x2 - 1/n2)
     
-    CI <- exp( ... + c(-1, 1) * ... * ...)
+    CI <- exp(log(or) + c(-1, 1) * z * log_or)
     return(round(CI, 4))
 }
 
@@ -11,13 +13,15 @@ RRCI <- function(x1, x2, n1, n2, alpha = 0.05) {
     p1 <- x1 / n1
     p2 <- x2 / n2
     rr <- p1 / p2
+    log_rr <- sqrt((1 - p1) / (x1) + (1 - p2) / (x2))
     z <- qnorm(1 - alpha / 2)
     
-    CI <- exp( ... + c(-1, 1) * ... * ...)
-    return(round(CI , 4))
+    CI <- exp(log(rr) + c(-1, 1) * z * log_rr)
+    return(round(CI, 4))
 }
 
-OR_CI <- ORCI(x1 = 9 , x2 = 5 , n1 = 27 , n2 = 27 )
-RR_CI <- RRCI(x1 = 9 , x2 = 5 , n1 = 27 , n2 = 27 )
+OR_CI <- ORCI(x1 = 9, x2 = 5, n1 = 27, n2 = 27)
+RR_CI <- RRCI(x1 = 9, x2 = 5, n1 = 27, n2 = 27)
 
 sol <- list(OR_CI = OR_CI, RR_CI = RR_CI)
+print(sol)
