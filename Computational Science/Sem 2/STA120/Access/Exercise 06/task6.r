71e8879935b34396e5e32008f9caca3182499717
Mert Erol <71695384+troniixx@users.noreply.github.com>
2024-04-07 13:51:36 +0200

    Update task6.r

--- Computational Science/Sem 2/STA120/Access/Exercise 06/task6.r
+++ Computational Science/Sem 2/STA120/Access/Exercise 06/task6.r
@@ -1,22 +1,22 @@
 ORCI <- function(x1, x2, n1, n2, alpha = 0.05) {
-    p1 <- x1 / n1
-    p2 <- x2 / n2
+    p1 <- x1 / (n1 - x1)
+    p2 <- x2 / (n2 - x2)
     z <- qnorm(1 - alpha / 2)
-    or <- (p1 / (1 - p1)) / (p2 / (1 - p2))
-    log_or <- sqrt(1/x1 - 1/n1 + 1/x2 - 1/n2)
+    log_se <- sqrt(1/x1 + 1/(n1-x1) + 1/x2 + 1/(n2-x2))
+    log_or <- log(p1) - log(p2)
     
-    CI <- exp(log(or) + c(-1, 1) * z * log_or)
+    CI <- exp(log_or + c(-1, 1) * z * log_se)
     return(round(CI, 4))
 }
 
 RRCI <- function(x1, x2, n1, n2, alpha = 0.05) {
     p1 <- x1 / n1
     p2 <- x2 / n2
-    rr <- p1 / p2
-    log_rr <- sqrt((1 - p1) / (x1) + (1 - p2) / (x2))
-    z <- qnorm(1 - alpha / 2)
+    z <- qnorm(1 - alpha/2)
+    log_rr <- log(p1) - log(p2)
+    log_se <- sqrt(1/x1 - 1/n1 + 1/x2 - 1/n2)
     
-    CI <- exp(log(rr) + c(-1, 1) * z * log_rr)
+    CI <- exp(log_rr + c(-1, 1) * z * log_se)
     return(round(CI, 4))
 }
 
