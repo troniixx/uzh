7cd9eeeacdec76339060946f2c120482be90fccf
Mert Erol <71695384+troniixx@users.noreply.github.com>
2024-04-08 18:31:52 +0200

    finished STA120 ex6

--- Computational Science/Sem 2/STA120/Access/Exercise 06/task5.r
+++ Computational Science/Sem 2/STA120/Access/Exercise 06/task5.r
@@ -3,10 +3,12 @@ mydata <- matrix(c(9, 5, 18, 22), nrow = 2, ncol = 2, byrow = TRUE,
                                 c("Treatment A", "Treatment B")))
 
 # null hypothesis: the proportions in both groups are the same.
-alpha = 0.05
-prop_p_val <- prop.test(mydata, conf.level = 1 - alpha)$p.value
-prop_ci <- fisher.test(mydata, conf.level = 1 - alpha)$conf.int
+prop_p_val <- prop.test(mydata)$p.value
+prop_ci <- prop.test(mydata)$conf.int
 
-sol <- list(prop_p_val = prop_p_val, prop_ci = prop_ci)
+fisher_p_val <- fisher.test(mydata)$p.value
+fisher_ci <- fisher.test(mydata)$conf.int
+
+sol <- list(prop_p_val = prop_p_val, prop_ci = prop_ci, fisher_p_val = fisher_p_val, fisher_ci = fisher_ci)
 
 print(sol)
