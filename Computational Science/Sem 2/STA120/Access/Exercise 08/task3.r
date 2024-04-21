2467d272a26eb6d0ad3b104e65e51fe7f9cd0419
Mert Erol <71695384+troniixx@users.noreply.github.com>
2024-04-21 13:43:57 +0200

    finished STA120.8

--- Computational Science/Sem 2/STA120/Access/Exercise 08/task3.r
+++ Computational Science/Sem 2/STA120/Access/Exercise 08/task3.r
@@ -1,21 +1,21 @@
-png(file = "solution.png")
-    require(mvtnorm)
-    set.seed(14)
-    mu <- c(1, 2)
-    sigma <- matrix(1, 1, 1, 2, byrow = TRUE, nrow = 2)
-    res <- ...
-    
-    # from (b)
-    par(mai = c(0.8, 0.8, 0.1, 0.1)) # to have a proper aspect ratio
-    plot(res, xlab = "y", ylab = "z", pch = 20,
-            xlim = c(-3, 7), ylim = c(-3, 7))
-
-    # exercise now
-    # Make sure you choose a right operator instead of "%%%".
-    points(... > ... "%%%" ... < ... , ... ], col = "red", pch = 20) #nolint
-    z <- ...
-    mu.constr <- ... # Use formula from the lecture.
-    sigma.constr <- ... # Use formula from the lecture.
-    y.constr <- ... # Generating 100 points with new mu and sigma
-    points(y.constr, rep(z, 100), col = "blue", pch = 20)
+png(file="solution.png")
+  require(mvtnorm)
+  set.seed(14)
+  mu <- c(1, 2)
+  sigma <- matrix(c(1, 1, 1, 2), byrow = TRUE, nrow = 2)
+  res <- rmvnorm(n = 500, mean = mu, sigma = sigma)
+  
+  # from (b)
+  par(mai = c(0.8, 0.8, 0.1, 0.1)) # to have a proper aspect ratio
+  plot(res, xlab = "y", ylab = "z", pch = 20,
+       xlim = c(-3, 7), ylim = c(-3, 7))
+  
+  # exercise now
+  # Make sure you choose a right operator instead of "%%%".
+  points(res[res[, 2] > 3 & res[, 2] < 4, ], col = "red", pch = 20)
+  z <- 3.5
+  mu.constr <- mu[1] + sigma[1, 2] * (1/sigma[2, 2]) * sigma[2, 1] # Use formula from the lecture.
+  sigma.constr <- sigma[1, 1] - sigma[1, 2] * (1/sigma[2, 2]) * sigma[2, 1] # Use formula from the lecture.
+  y.constr <- rnorm(100, mu.constr, sqrt(sigma.constr)) # Generating 100 points with new mu and sigma
+  points(y.constr, rep(z, 100), col = "blue", pch = 20)
 dev.off()
