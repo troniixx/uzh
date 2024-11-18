ea20d52bb8e2552bf48c4a90689d102a93e00ca8
Mert Erol <meroert55@gmail.com>
2024-11-18 13:11:50 +0100

    started working on phy231.6

--- Computational Science/Sem 3/PHY231/Exercise 6/skeleton_sheet6.py
+++ Computational Science/Sem 3/PHY231/Exercise 6/skeleton_sheet6.py
@@ -1,21 +1,12 @@
-"""Skeleton for Data Analysis Fall 2022 sheet 5.
-
-This sheet helps to structure the code for the exercises of sheet 5.
-It shows idioms and best practices for writing code in Python.
-
-You can directly use this sheet and modify it.
-
-(It is not guaranteed to be bug free)
-"""
 import numpy as np
 import scipy.optimize as opt
 from matplotlib import pyplot as plt
 
 
 def current_ohmslaw(U, R):
-    r"""Calculate the current according to Ohm's Law given the voltage U and resistance R.
+    r"""Calculate the current according to Ohm"s Law given the voltage U and resistance R.
 
-    Ohm's Law states that the current is given by:
+    Ohm"s Law states that the current is given by:
 
     .. math::
 
@@ -29,13 +20,13 @@ def current_ohmslaw(U, R):
         float or array: Value of the linear function. Shape is the broadcast shape of
             the inputs.
     """
-    pass  # TODO: implement ohm's law.
+    return U / R  # Implemented Ohm"s Law: I = U / R
 
 
 def current_ohmslaw_bias(U, R, bias=None):
-    """Calculate the current according to Ohm's Law given the voltage U and resistance R with a bias.
+    """Calculate the current according to Ohm"s Law given the voltage U and resistance R with a bias.
 
-    Ohm's Law states that the current is given by:
+    Ohm"s Law states that the current is given by:
 
     .. math::
 
@@ -58,7 +49,7 @@ def current_ohmslaw_bias(U, R, bias=None):
     """
     if bias is None:
         bias = 0  # with this, we can also use the function without bias.
-    pass  # implement ohm's law with bias. (try to re-use code from above by calling the function (NOT by copy paste))
+    return current_ohmslaw(U, R + bias)  # Reuse code from above by calling the function
 
 
 def chi2(x, y, err):
@@ -72,14 +63,33 @@ def chi2(x, y, err):
     Returns:
         float: The chi2 statistic.
     """
-    pass  # TODO: implement chi2.
+    return np.sum(((x - y) / err) ** 2)  # Implemented chi2 calculation
 
+DATA = np.loadtxt("/Users/merterol/uzh/Computational Science/Sem 3/PHY231/Exercise 6/current_measurements.txt")  # Load the data from the file
+DATA_UNCERT = np.loadtxt("/Users/merterol/uzh/Computational Science/Sem 3/PHY231/Exercise 6/current_measurements_uncertainties.txt")  # Load the uncertainties from the file
+
+voltage = DATA[:, 0]  # Extract the voltage from the data
+current = DATA[:, 1]  # Extract the current from the data
+uncertanties = DATA_UNCERT[:, 2]  # Extract the uncertainties from the data
 
 def ex_1a():
     """Run exercise 1a."""
 
-    # Here is your code for exercise 1a.
-    print("ex1a executed.")
+    # Plot the current as a function of voltage (without the trendline)
+    plt.scatter(voltage, current, color="blue", label="Measured data", marker="o")
+
+    # Add labels and title
+    plt.xlabel("Voltage (V)")
+    plt.ylabel("Current (I)")
+    plt.title("Current vs Voltage")
+    plt.grid(True)
+
+    # Display a legend
+    plt.legend()
+    plt.savefig("/Users/merterol/uzh/Computational Science/Sem 3/PHY231/Exercise 6/current_vs_voltage.pdf", dpi=300)
+
+    plt.close()
+    print("ex1a executed and plot saved.")
 
 
 # This is an example of creating 1b composing different functions together.
@@ -87,28 +97,45 @@ def chi2_1b(R):
     """Calculate chi2 in dependence of the resistance."""
 
     # Here is your code for exercise 1b.
-    data = ...  # load it from file for example
-    voltage = data[:, 0]  # etc.
-    current_pred = current_ohmslaw(...)
-    chi2val = chi2(...)
+    current_pred = current_ohmslaw(voltage, R)
+    chi2val = chi2(current, current_pred, uncertanties)
     return chi2val
 
 
 def ex_1c():
     """Run exercise 1c."""
 
-    # Here is your code for exercise 1c.
-    resistances = np.linspace(...)  # start, stop, number of steps
-    chi2val = chi2_1b(...)
-    # maybe use a for-loop to calculate chi2 for different resistances.
+    # Define the range of resistances
+    resistances = np.linspace(1, 100, 500)  # You can adjust start, stop, and number of steps
 
+    # Calculate chi2 for each resistance value
+    chi2_values = [chi2_1b(R) for R in resistances]  # List of chi2 values for each resistance
 
-    # plot the chi2 value as a function of the resistance.
-    plt.figure()  # ALWAYS create a new figure before plotting.
-    plt.plot(...)
-    ...  # don't forget to add the legend, labels, titles etc.
+    # Plot the chi2 value as a function of the resistance
+    plt.figure()  # Create a new figure
+
+    plt.plot(resistances, chi2_values, label=r"$\chi^2$ vs. Resistance")
+
+    # Highlight the minimum chi2 value and corresponding resistance
+    min_chi2 = min(chi2_values)
+    best_R = resistances[np.argmin(chi2_values)]
+    plt.scatter(best_R, min_chi2, color="red", label=f"Minimum $\\chi^2$ at R = {best_R:.2f} Ohms")
+
+    # Add labels, title, and legend
+    plt.xlabel("Resistance (Ohms)")
+    plt.ylabel(r"$\chi^2$")
+    plt.title(r"$\chi^2$ vs. Resistance")
+    plt.grid(True)
+    plt.legend()
+
+    # Save the plot as a PNG file
     plt.savefig("ex1c.png")
-    plt.close()  # ALWAYS close the figure when not needed anymore
+
+    # Close the figure
+    plt.close()
+
+    # Print the best fit resistance value and chi^2
+    print(f"Best fit resistance: {best_R:.2f} Ohms with chi^2 = {min_chi2:.2f}")
 
 
 def ex_2g():
@@ -124,10 +151,10 @@ def ex_2g():
     print("ex2g executed.")
 
 
-if __name__ == '__main__':
-    # You can uncomment the exercises that you don't want to run. Here we have just one,
+if __name__ == "__main__":
+    # You can uncomment the exercises that you don"t want to run. Here we have just one,
     # but in general you can have more.
-    ex_1a()
+    #ex_1a()
     ex_1c()
-    ex_2g()
-    plt.show()  # comment out when submitting
+    #ex_2g()
+    #plt.show()  # comment out when submitting
