835456fce31ce3562bf612863212b1818564b0ca
troniixx <meroert55@gmail.com>
2025-03-03 12:14:30 +0100

    working on phy124

--- Computational Science/Sem 4/PHY124/Exercise 3/compute_pi.py
+++ Computational Science/Sem 4/PHY124/Exercise 3/compute_pi.py
@@ -1,2 +1,13 @@
-def arctan(x):
-    pass
+def arctan(x, terms=5000):
+    term = x
+    arctan_sum = term
+    
+    for k in range(1, terms):
+        term *= -x**2
+        arctan_sum += term / (2*k + 1)
+    
+    return arctan_sum
+
+
+sol = str(4*(arctan(1/2, 15000) + arctan(1/3, 15000)))
+print(sol)
