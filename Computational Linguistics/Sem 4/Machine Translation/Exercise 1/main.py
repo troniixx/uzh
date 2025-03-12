be1b17787f201daf9f006a7134906f56b9de9163
Mert Erol <meroert55@gmail.com>
2025-03-12 14:29:40 +0100

    working on MT

--- Computational Linguistics/Sem 4/Machine Translation/Exercise 1/main.py
+++ Computational Linguistics/Sem 4/Machine Translation/Exercise 1/main.py
@@ -1,11 +1,18 @@
 import part1 as p1
+from sys import argv
     
 if __name__ == "__main__":
-    HYP = "/Users/merterol/Desktop/VSCode/uzh/Computational Linguistics/Sem 4/Machine Translation/Exercise 1/test_files/test_hypothesis.txt"
-    REF = "/Users/merterol/Desktop/VSCode/uzh/Computational Linguistics/Sem 4/Machine Translation/Exercise 1/test_files/test_reference.txt"
+    args = argv
+    if len(args) != 4:
+        print("Usage: python main.py <hypothesis_file> <reference_file> <ngram>")
+        exit(1)
+        
+    HYP = args[1]
+    REF = args[2]
+    N = int(args[3])
 
     print(p1.reader(HYP))
-    print(p1.get_ngrams(HYP, 3))
-    print(p1.get_ngrams(REF, 3))
+    print(p1.get_ngrams(HYP, N))
+    print(p1.get_ngrams(REF, N))
 
-    print(p1.ngram_precision(HYP, REF, 2))
+    print(p1.ngram_precision(HYP, REF, N))
