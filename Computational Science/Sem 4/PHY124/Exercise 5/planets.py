f037328e5ce9dc771fe6ec51d8ad35bc3986633d
troniixx <meroert55@gmail.com>
2025-03-17 12:00:01 +0100

    added cli to planets.py (phy124)

--- Computational Science/Sem 4/PHY124/Exercise 5/planets.py
+++ Computational Science/Sem 4/PHY124/Exercise 5/planets.py
@@ -1,23 +1,33 @@
 import pandas as pd
+from sys import argv
 
-exoplanets = pd.read_csv("/Users/merterol/Desktop/VSCode/uzh/Computational Science/Sem 4/PHY124/Exercise 5/exoplanet.eu_catalog.csv")
+def process(input):
+    exoplanets = pd.read_csv(input)
 
-T_sun = 5800  # K
+    T_sun = 5800  # K
 
-exoplanets['daylight_strength'] = (
-    (exoplanets['star_radius'])**2 * 
-    (exoplanets['star_teff'] / T_sun)**4 * 
-    (exoplanets['semi_major_axis'])**(-2)
-)
+    exoplanets["daylight_strength"] = (
+        (exoplanets["star_radius"])**2 * 
+        (exoplanets["star_teff"] / T_sun)**4 * 
+        (exoplanets["semi_major_axis"])**(-2)
+    )
 
-exoplanets['mass_earth'] = exoplanets['mass'] * 320
+    exoplanets["mass_earth"] = exoplanets["mass"] * 320
 
-earth_like = exoplanets[
-    (exoplanets['mass_earth'] > 0.5) & 
-    (exoplanets['mass_earth'] < 1.5) & 
-    (exoplanets['daylight_strength'] > 0.5) & 
-    (exoplanets['daylight_strength'] < 1.5)
-]
+    earth_like = exoplanets[
+        (exoplanets["mass_earth"] > 0.5) & 
+        (exoplanets["mass_earth"] < 1.5) & 
+        (exoplanets["daylight_strength"] > 0.5) & 
+        (exoplanets["daylight_strength"] < 1.5)
+    ]
 
 
-print(earth_like.head(10))
+    return earth_like.head(10)
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print("Usage: python planets.py <input_file>")
+        exit(1)
+
+    IN = argv[1]
+    print(process(IN))
